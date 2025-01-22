64fb55cd1c0ade8e6d50fca9ad45856127ace79c
HugoSanche <hbmclaren@gmail.com>
2025-01-21 21:51:15 -0600

    Example of iterate Enums

---
+++ untitled/src/IterateEnum.java
@@ -0,0 +1,3 @@
+public enum IterateEnum {
+    LUNES,MARTES,MIERCOLES,JUEVES,VIERNES,SABADO,DOMINGO
+}
---
+++ untitled/src/TestIterateEnum.java
@@ -0,0 +1,8 @@
+public class TestIterateEnum {
+    public static void main(String[] args) {
+        for(IterateEnum day:IterateEnum.values()){
+            System.out.println(day);
+        }
+
+    }
+}
